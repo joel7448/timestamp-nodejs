43cb46f312870e6fc2f0ca7f01446e122ebff1c5
joel7448 <joel.joel52@gmail.com>
2022-08-02 10:00:20 +0530

    timestamp

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const date= new Date();
+
+app.get("/createtimestamp",function (req,res){
+    fs.writeFile(`./${date.getDate()}-${date.getMonth()}-${date.getFullYear()}-${date.getHours()}.txt`,`Date:${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} 
+    timestamp : ${new Date()}`,(err)=>{
+        if(err) throw err;
+        console.log("File created");
+    })
+    res.json("file-created")
+})
+
+app.listen(3000);
